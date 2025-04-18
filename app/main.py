6061c45fd21a25a4e0c8f5ded76ca8ca40fc0a40
Krystian Wojtiuk <krystianwwork@gmail.com>
2025-04-18 13:23:35 +0100

    v1

--- app/main.py
+++ app/main.py
@@ -1,1 +1,12 @@
-# write your code here
+def copy_file(command: str) -> None:
+    names = command.split(" ")
+    if len(names) == 3:
+        if names[1] == names[2]:
+            return
+        if names[0] == "cp":
+            try:
+                with (open(names[1], "r") as file_in,
+                      open(names[2], "w") as file_out):
+                    file_out.write(file_in.read())
+            except FileNotFoundError:
+                print("File not found")
